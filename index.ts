d93a5ffebb1d9e879d4ecd36e7be7bae9dbba7b2
Swapnil Ingle <ingleswapnil2004@gmail.com>
2026-06-17 22:02:09 +0530

    feat: add Vercel configuration for deployment and update server start logic

--- index.ts
+++ index.ts
@@ -22,6 +22,10 @@ app.use('/auth', authRoutes);
 
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}
+
+export default app;
